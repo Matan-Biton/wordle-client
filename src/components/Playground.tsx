277b27489fdfe29384cc02bd578d18ba28093ee0
Matan-Biton <Matan.bit97@gmail.com>
2022-12-14 09:30:24 +0200

    moved to another directory

--- src/components/Playground.tsx
+++ src/components/Playground.tsx
@@ -2,17 +2,16 @@ import { useId, useContext } from "react";
 import { wordleContext } from "../providers/wordleContext";
 
 export function Playground() {
-  const wApi = useContext(wordleContext);
-  const { inputsGrid } = wApi;
+  const {inputsGrid} = useContext(wordleContext);
 
   return (
     <div className="grid">
-      {inputsGrid.map((row) => (
-        <div className="attempt">
+      {inputsGrid.map((row, i) => (
+        <form className="attempt" key={i} id={`${i + 1}`}>
           {row.map((char) => (
             <input key={useId()} type="text" className="tile" value={char} readOnly />
           ))}
-        </div>
+        </form>
       ))}
     </div>
   );
